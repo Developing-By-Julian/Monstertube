81849d05178f1fb0118e0f30f3d8b74349d79212
SmidtDeveloping <101642623+SmidtDeveloping@users.noreply.github.com>
2024-05-17 21:57:28 +0200

    DWA

--- dashboard/routers/leaderboard.js
+++ dashboard/routers/leaderboard.js
@@ -3,12 +3,15 @@ const schema = require("../../db/money").User
 const client = require("../../src/botClient")
 Router.get("/dashboard/leaderboard", async (req, res ) => {
     const guildID = req.session.guildid
+    console.log(`Guildid: ${guildID}`);
     const guild = client.guilds.cache.get(guildID)
+    console.log(`Guild: ${guild}`);
     // const leaderboard = await schema.find().limit(10).sort({$sum: ['$cash', '$bank']})
     const lb = await schema.aggregate([
         {
             $project: {
-                total: {$sum : ['$cash', '$bank']}
+                total: {$sum : ['$cash', '$bank']},
+                userId: { $toInt: '$userId' } 
             }
         },
         {
@@ -20,8 +23,9 @@ Router.get("/dashboard/leaderboard", async (req, res ) => {
     ])
     const users = []
     for (const user of lb) {
+        console.log(user);
         const user_fetch = guild.members.cache.get(user.userId)
-
+        console.log(`user fetch ${user_fetch}`);
         users.push({user, user_fetch})
     }
     console.log(users);
--- dashboard/routers/login_uit.js
+++ dashboard/routers/login_uit.js
@@ -13,13 +13,15 @@ app.get("/login", (req, res) => {
 
 app.post('/login', async (req, res) => {
     const { username, password } = req.body;
-    const user = await Loginschema.findOne({ username });
+    const user = await Loginschema.findOne({ username: username });
+    console.log(user);
 var guildid
-    if (user.username === "webbeheer") {
+    if (username === "julian@monstertube.nl") {
         guildid = "1233925574070767696"
-    } else if (user.username === "monstertube") {
+    } else if (username === "monstertube") {
         guildid = "1230258666146365481"
     }
+    console.log(guildid);
     if (user && username === user.username && password === user.password) {
         req.session.user = user;
 		req.session.guildid = guildid
@@ -35,40 +37,57 @@ app.get('/logout', (req, res) => {
     res.redirect('/');
 });
 
-app.get("/auth", (req, res) => {
+app.get("/auth/redirect", async (req, res) => {
     res.render("auth")
 })
-app.post("/auth", (req, res) => {
-const {value, code} = req.body
-if (code === "1986") {
-    console.log(`${code}, Code is goed `);
-    res.redirect(`/auth/change?email=${encodeURIComponent(value)}`);
-}  else {
-    console.log(`${code}, Code is fout `);
-     res.redirect("/")
-}
 
+app.post("/auth/redirect", async (req, res) => {
+    const email = req.body.value
 
-
-
+    res.redirect(`/auth/change?email=${email}`)
 })
-app.get("/auth/change", (req, res) => { 
-        res.render("password")
+
+
+app.get("/auth/change", async (req, res) => { 
+    let Fdata
+    const find = await Loginschema.findOne({username: req.query.email }).then(data => {console.log(data); Fdata = data})
+
+        res.render("password", {data: {find: Fdata}})
     
 })
 app.post("/auth/change", async (req, res) => {
-const {value} = req.body
+const {current_password, new_password, confirm_password} = req.body
 const email = req.query.email
+let Fdata
 
-const find = Loginschema.findOne({username: email })
-console.log(find);
-if (!find) {
-    return res.status(500).send("Geen data gevonden ");
-} {
-   Loginschema.findOneAndUpdate({username: email}, {$set: {password: value}}, {new: true}).then(newuser => console.log(newuser)).finally(res.redirect("/"))
+const find = await Loginschema.findOne({username: email }).then(data => {console.log(data); Fdata = data})
+
+
+
+if (!Fdata) {
+    return res.status(500).send("Geen data gevonden, change ");
 }
 
+if (!current_password === Fdata.password) {
+    return res.status(500).send("Wachtwoord komt niet overheen met bestaand wachtwoord");
+}
 
+if (!new_password === confirm_password) {
+    return res.status(500).send("Nieuw wachtwoord komt niet overheen met bevestigings wachtwoord");
+}
+
+Loginschema.findOneAndUpdate(
+    {username: email},
+    {$set: {password: new_password}},
+    {new: true}
+)
+.then(updated => {
+    if (updated) {
+        res.status(500).send("Nieuw wachtwoord ingesteld");
+    } else {
+        return res.status(500).send("Geen data gevonden ");
+    }
+})
 
 
 
